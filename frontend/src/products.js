1900ef736019a59f3975644aba1cadbcbcf03ced
Connor Dillon <connor@codewithconnor.com>
2020-10-12 17:50:43 -0700

    :rocket: Created product page and products card using example data

---
+++ frontend/src/products.js
@@ -0,0 +1,81 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Banana Phone',
+    image: '/images/banana.jpg',
+    description:
+      "Bluetooth technology lets you connect the Banana Phone with compatible devices wirelessly and instantly. High-quality AAC audio offers an immersive listening experience. Built-in microphone allows you to take calls while eating your lunch - just don't eat the wrong banana!",
+    brand: 'Banana Phone Emporium',
+    category: 'Electronics',
+    price: 1.25,
+    countInStock: 325,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Rubber Duck De-Bugger 9000',
+    image: '/images/rubberduck.jpg',
+    description:
+      'Introducing the latest in rubber duck debugging technology. The De-Bugger 9000 offers an industry transformative 512MB stepped debugging cache and an omni-directional cookie deployment system, our rubber duck debugging tool adds tons of capability without complexity. An unprecedented leap towards a slightly less buggy future.',
+    brand: "Ducks'R'Us",
+    category: 'Electronics',
+    price: 1899.99,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Egg Cloaking Device',
+    image: '/images/eggs.jpg',
+    description:
+      "Tired of your eggs looking like eggs? Our Egg Cloaking Device is the solution to all of your problems! This remarkable device will ensure no one knows where you've put your eggs - even if you've put them all in one basket! Hiding your eggs from a wild roaming herd of bloodthirsty alpacas has never been easier!",
+    brand: 'The Great Egg Company',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 5,
+    rating: 1.5,
+    numReviews: 33,
+  },
+  {
+    _id: '4',
+    name: 'The Bean',
+    image: '/images/greenbean.jpg',
+    description:
+      'Everyone knows the secret to a state-of-the-art home entertainment system are the beans you use. The Bean takes things to a new level, by doing exactly what you need it to do - always.',
+    brand: 'Beantronics',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 107,
+    rating: 5,
+    numReviews: 80,
+  },
+  {
+    _id: '5',
+    name: 'Lemon Slice',
+    image: '/images/lemon.jpg',
+    description: "This is just a lemon slice... Or is it? Will your curiousity get the better of you? We know you'll make the right choice.",
+    brand: 'LimonLimon',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 70,
+    rating: 3.5,
+    numReviews: 42,
+  },
+  {
+    _id: '6',
+    name: 'Pre-Pickle Slice',
+    image: '/images/pickleslice.jpg',
+    description:
+      'Meet Pre-Pickle Slice. Ever wonder where all of your pickles come from? Look no further, friend. This is the latest and greatest in pickle technology. These pre-pickles are harvested using eco-friendly, artisnal Lorentzian wormholes to travel back in time (we usually aim for last Wednesday, but sometimes our time travelers get stuck in non-Euclidean space and end up being consumed by the dreadful creatures of the void) - before your pickle ever became a pickle. From a name you can trust: Pickles & Co.',
+    brand: 'Pickles & Co.',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 8126,
+    rating: 4,
+    numReviews: 129,
+  },
+]
+
+export default products
