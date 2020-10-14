89e409a9aa5e46ee22d1af0cd10efc6fc22ab971
Connor Dillon <connor@codewithconnor.com>
2020-10-13 17:12:58 -0700

    :sparkles: Added react-router, refactored to SPA

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <a href={`/product/${product.id}`}>
+      <Link to={`/product/${product.id}`}>
         <Card.Img src={product.image} variant='top' />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product.id}`}>
+        <Link to={`/product/${product.id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as='div'>
           <Rating value={product.rating} text={`${product.numReviews} reviews`} />
         </Card.Text>
