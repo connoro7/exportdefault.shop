4718af597d6a923f6c921e47f2e1a3648c372e0e
Connor Dillon <connor@codewithconnor.com>
2020-11-09 13:48:05 -0800

    :tada: Moved dev products to backend, routed frontend to backend via axios

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product.js'
-import products from '../products'
 import { Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+    }
+
+    fetchProducts()
+  }, [])
   return (
     <>
       <h1>Latest Products</h1>
