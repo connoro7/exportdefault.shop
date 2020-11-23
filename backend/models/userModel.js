3de5b58793cbb7d52c84332f18124c3d27a08b87
Connor Dillon <connor@codewithconnor.com>
2020-11-23 15:23:02 -0800

    :file_folder: Added 1st draft the user data model

---
+++ backend/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: String,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const User = mongoose.model('User', userSchema)
+
+export default User
