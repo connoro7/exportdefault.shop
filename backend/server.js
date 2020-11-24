07561f8d7ec451348cd9ef2dd90a59d819391443
Connor Dillon <connor@codewithconnor.com>
2020-11-24 14:53:06 -0800

    :package: Implemented custom error handler middleware for the API

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 
@@ -11,6 +12,10 @@ connectDB()
 const app = express()
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
