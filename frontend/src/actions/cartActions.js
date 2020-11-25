bcc99800da2fdd0c7d721627f92c734237e3dd8d
Connor Dillon <connor@codewithconnor.com>
2020-11-25 15:22:41 -0800

    :star: Added cart reducer, add-to-cart action, implemented localstorage for cart

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,19 @@
+import { axios } from 'axios'
+import { CART_ADD_ITEM } from '../constants/cartConstants'
+
+export const addToCart = (id, quantity) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`)
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      product: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      quantity,
+    },
+  })
+
+  localStorage.setItems('cartItems', JSON.stringify(getState().cart.cartItems))
+}
