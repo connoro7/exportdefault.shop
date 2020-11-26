db31c2ce62d66df043a6bcfba72756a1e0a7744b
Connor Dillon <connor@codewithconnor.com>
2020-11-25 16:09:40 -0800

    :white_check_mark: Connected Cart state in redux to DOM

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -13,6 +13,11 @@ const CartScreen = ({ match, location, history }) => {
 
   const dispatch = useDispatch()
 
+  const cart = useSelector((state) => state.cart)
+  const { cartItems } = cart
+
+  console.log(cartItems)
+
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, quantity))
