9ac94c36506b454c5829ec948b436cdb59c24569
Connor Dillon <connor@codewithconnor.com>
2020-12-09 15:17:22 -0800

    :white_check_mark: Added user registration state and actions to Redux

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAILED, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from '../constants/userConstants'
+import { USER_LOGIN_FAILED, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAILED, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
@@ -7,6 +7,22 @@ export const userLoginReducer = (state = {}, action) => {
     case USER_LOGIN_SUCCESS:
       return { loading: false, userInfo: action.payload }
     case USER_LOGIN_FAILED:
+      return { loading: false, error: action.payload }
+    case USER_LOGOUT:
+      return {}
+
+    default:
+      return state
+  }
+}
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true }
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload }
+    case USER_REGISTER_FAILED:
       return { loading: false, error: action.payload }
     case USER_LOGOUT:
       return {}
