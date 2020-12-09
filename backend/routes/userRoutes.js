6f1509aca9c9aafb306a8f0e9d2801f049c3ebb8
Connor Dillon <connor@codewithconnor.com>
2020-12-09 13:00:58 -0800

    :floppy_disk: Added API route to create new user, implemented password salt + hash

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import { authUser, createUser, getUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
+router.route('/').post(createUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
