a5dd649039a43e9614ddc7c7a44234cd46aa861a
Connor Dillon <connor@codewithconnor.com>
2020-12-09 14:58:06 -0800

    :star: User logout action now reflected in UI as well as Redux

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { logout } from '../actions/userActions'
 
 const Header = () => {
   const dispatch = useDispatch()
@@ -9,7 +10,7 @@ const Header = () => {
   const { userInfo } = userLogin
 
   const logoutHandler = () => {
-    console.log('logout')
+    dispatch(logout())
   }
 
   return (
