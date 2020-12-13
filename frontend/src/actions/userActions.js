ac181466a3ec1208e2dbd57aa332008018c42cf7
Connor Dillon <connor@codewithconnor.com>
2020-12-12 17:14:17 -0800

    :star: Added profile screen to UI, edit profile from frontend

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,16 @@
 import axios from 'axios'
-import { USER_LOGIN_FAILED, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAILED, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from '../constants/userConstants'
+import {
+  USER_DETAILS_FAILED,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_LOGIN_FAILED,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_REGISTER_FAILED,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+} from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -62,6 +73,37 @@ export const register = (name, email, password) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAILED,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    })
+  }
+}
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.get(`/api/users/${id}`, config)
+
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAILED,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     })
   }
