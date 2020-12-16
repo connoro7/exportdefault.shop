791904b776dfd3256ed0ffb9d0b8521ee7f9d94b
Connor Dillon <connor@codewithconnor.com>
2020-12-15 21:08:37 -0800

    :+1: Added redux handlers and localStorage for shipping info

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -19,8 +19,10 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(loca
 
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage },
   userLogin: { userInfo: userInfoFromStorage },
 }
 
