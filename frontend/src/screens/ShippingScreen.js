e129c6cee59a862c1baef5619df44b55fcd84af1
Connor Dillon <connor@codewithconnor.com>
2020-12-16 15:06:45 -0800

    :package: Finished implementing shipping actions and localStorage to shipping screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,18 +2,22 @@ import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from '../components/FormContainer'
-import { register } from '../actions/userActions'
-import { clearMessage } from '../actions/utilityActions'
+import { saveShippingAddress } from '../actions/cartActions'
 
 const ShippingScreen = ({ history }) => {
-  const [address, setAddress] = useState('')
-  const [city, setCity] = useState('')
-  const [postalCode, setPostalCode] = useState('')
-  const [country, setCountry] = useState('')
+  const cart = useSelector((state) => state.cart)
+  const { shippingAddress } = cart
+  const [address, setAddress] = useState(shippingAddress.address)
+  const [city, setCity] = useState(shippingAddress.city)
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
+  const [country, setCountry] = useState(shippingAddress.country)
+
+  const dispatch = useDispatch()
 
   const submitHandler = (e) => {
     e.preventDefault()
-    console.log('submit')
+    dispatch(saveShippingAddress({ address, city, postalCode, country }))
+    history.push('/payment')
   }
 
   return (
