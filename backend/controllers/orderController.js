646897b9e9ad4d87ee786d1bf68398557a5b0a42
Connor Dillon <connor@codewithconnor.com>
2020-12-28 15:19:35 -0800

    :star: Created controller for adding order items to user in database

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,28 @@
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
+
+/**
+ * Create new order
+ * @route POST /api/orders
+ * @access Private
+ * @protected
+ */
+const addOrderItems = asyncHandler(async (request, response) => {
+  const { orderItems, shippingAddress, paymentMethod, subtotal, shippingPrice, taxPrice, totalPrice} = request.body
+
+  // Bad request: orderItems has been initialized but length = 0
+  if(orderItems && orderItems === 0) {
+      response.status(400)
+      throw new Error('No order items')
+      return
+  } else { // Else, create a new order in the database
+    const order = new Order({
+      orderItems, shippingAddress, paymentMethod, subtotal, shippingPrice, taxPrice, totalPrice, user: request.user._id
+    })
+    const createdOrder = await order.save()
+
+    response.status(201).json(createdOrder)
+  }
+
+  response.json(products)
+})
