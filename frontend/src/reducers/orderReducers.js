8486ca8b57b0323a57131d44e4f5f4e2d7d2bf93
Connor Dillon <connor@codewithconnor.com>
2020-12-28 15:51:10 -0800

    :+1: Added order reducer to redux

---
+++ frontend/src/reducers/orderReducers.js
@@ -0,0 +1,19 @@
+import { ORDER_CREATE_REQUEST  } from '../constants/orderConstants'
+
+export const orderCreateReducer = (state = {}, action) => {
+  switch(action.type) {
+    case ORDER_CREATE_REQUEST:
+      return { loading: true }
+      
+    case ORDER_CREATE_SUCCESS:
+      return { loading: false, success: true, order: action.payload}
+    case ORDER_CREATE_FAILED:
+      return {
+        loading: false,
+        error: action.payload
+      }
+    default: 
+      return state
+
+  }
+}
