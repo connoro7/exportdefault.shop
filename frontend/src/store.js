0e3d5bca65eab0840f031162cf6bc73f4de06315
Connor Dillon <connor@codewithconnor.com>
2020-12-30 17:15:48 -0800

    :floppy_disk: Added paymentMethod read/write to localStorage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -23,8 +23,10 @@ const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localS
 
 const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
 
+const paymentMethodFromStorage = localStorage.getItem('paymentMethod') ? JSON.parse(localStorage.getItem('paymentMethod')) : []
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage },
+  cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage, paymentMethod: paymentMethodFromStorage },
   userLogin: { userInfo: userInfoFromStorage },
 }
 
