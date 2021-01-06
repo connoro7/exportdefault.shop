f956a6f8e1a30e046ce916f3319c0dbf2f83ba78
Connor Dillon <connor@codewithconnor.com>
2021-01-06 14:31:35 -0800

    :star: Added action for handling order payments

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,5 +1,15 @@
 import axios from 'axios'
-import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAILED, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAILED } from '../constants/orderConstants'
+import {
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_FAILED,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAILED,
+  ORDER_PAYMENT_REQUEST,
+  ORDER_PAYMENT_SUCCESS,
+  ORDER_PAYMENT_FAILED,
+} from '../constants/orderConstants'
 
 export const createOrder = (order) => async (dispatch, getState) => {
   try {
@@ -57,6 +67,37 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: ORDER_DETAILS_FAILED,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    })
+  }
+}
+
+export const payOrder = (orderId, paymentResult) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_PAYMENT_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(`/api/orders/${orderId}/pay`, paymentResult, config)
+
+    dispatch({
+      type: ORDER_PAYMENT_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: ORDER_PAYMENT_FAILED,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     })
   }
