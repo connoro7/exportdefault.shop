701c2f5ede6f796142d7096dff69a1c856073fa6
Connor Dillon <connor@codewithconnor.com>
2021-01-11 15:38:27 -0800

    Added empty /admin screen for security and UX

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
+import AdminPanelHomeScreen from './screens/AdminPanelHomeScreen'
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/admin' component={AdminPanelHomeScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/order/:id' component={OrderScreen} />
           <Route path='/login' component={LoginScreen} />
