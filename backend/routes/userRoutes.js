568566b37890fcbb5a5b072ddde0f32dff16fbfe
Connor Dillon <connor@codewithconnor.com>
2021-01-11 13:20:26 -0800

    :sparkles: Added endpoint to API to get a list of all users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, createUser, getUserProfile, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser, createUser, getAllUsers, getUserProfile, updateUserProfile } from '../controllers/userController.js'
+import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
-router.route('/').post(createUser)
+router.route('/').post(createUser).get(protect, isAdmin, getAllUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
