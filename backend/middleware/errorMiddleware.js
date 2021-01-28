5374852b05c0dda1d9a3780fb7edd943d494185a
Connor Dillon <connor@codewithconnor.com>
2021-01-28 10:34:57 -0800

    :bug: Fixed bug in the errorHandler middleware, go figure - fixes #93

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -16,8 +16,7 @@ const notFound = (req, res, next) => {
  */
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode
-  res.status(statusCode)
-  res.json({
+  res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   })
