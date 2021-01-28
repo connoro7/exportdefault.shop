53e3929a8e2497a80a0383e5e5325a698a7d573e
Connor Dillon <connor@codewithconnor.com>
2021-01-28 11:22:19 -0800

    :sparkles: Implemented creating product reviews into Redux: actions, reducers

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -15,6 +15,9 @@ import {
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAILED,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAILED,
 } from '../constants/productConstants'
 
 export const listProducts = () => async (dispatch) => {
@@ -138,6 +141,41 @@ export const updateProduct = (product) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_UPDATE_FAILED,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    })
+  }
+}
+
+/**
+ * 
+ * @param {*} productId 
+ * @param {object} review {rating, comment} 
+ */
+export const createProductReview = (productId, review) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    await axios.post(`/api/products/${productId}/reviews`, review, config)
+
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_SUCCESS,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_CREATE_REVIEW_FAILED,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     })
   }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -16,6 +16,10 @@ import {
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAILED,
   PRODUCT_UPDATE_RESET,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAILED,
+  PRODUCT_CREATE_REVIEW_RESET,
 } from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -75,15 +79,30 @@ export const productCreateReducer = (state = {}, action) => {
   }
 }
 
-export const productUpdateReducer = (state = {}, action) => {
+export const productUpdateReducer = (state = { product: {} }, action) => {
   switch (action.type) {
     case PRODUCT_UPDATE_REQUEST:
       return { loading: true }
     case PRODUCT_UPDATE_SUCCESS:
-      return { loading: false, success: true }
+      return { loading: false, success: true, product: action.payload }
     case PRODUCT_UPDATE_FAILED:
       return { loading: false, error: action.payload }
     case PRODUCT_UPDATE_RESET:
+      return { product: {} }
+    default:
+      return state
+  }
+}
+
+export const productCreateReviewReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REVIEW_REQUEST:
+      return { loading: true }
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return { loading: false, success: true }
+    case PRODUCT_CREATE_REVIEW_FAILED:
+      return { loading: false, error: action.payload }
+    case PRODUCT_CREATE_REVIEW_RESET:
       return {}
     default:
       return state
