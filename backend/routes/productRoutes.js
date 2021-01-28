4e7e8abcf98cc90ba9ea44ea3fb22aa6b5031657
Connor Dillon <connor@codewithconnor.com>
2021-01-28 11:19:10 -0800

    :sparkles: Added controller and server routes for creating product reviews

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { getProducts, getProductById, deleteProduct, updateProduct, createProduct } from '../controllers/productController.js'
+import { getProducts, getProductById, deleteProduct, updateProduct, createProduct, createProductReview } from '../controllers/productController.js'
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, isAdmin, createProduct)
 router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct).put(protect, isAdmin, updateProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
 
 export default router
