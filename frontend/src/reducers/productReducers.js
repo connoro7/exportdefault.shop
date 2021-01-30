1e92bf8e3fd527c5d06eab5ba29de12173c7503c
Connor Dillon <connor@codewithconnor.com>
2021-01-29 16:26:28 -0800

    ✨ Integrated pagination into Products state

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -27,10 +27,10 @@ export const productListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] }
     case PRODUCT_LIST_SUCCESS:
-      return { loading: false, products: action.payload }
+      // returning 
+      return { loading: false, products: action.payload.products, pages: action.payload.pages, page: action.payload.page }
     case PRODUCT_LIST_FAILED:
       return { loading: false, error: action.payload }
-
     default:
       return state
   }
