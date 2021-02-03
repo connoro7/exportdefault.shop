e6f27f15d902ffc3beb4708f02570360fbd8b264
Connor Dillon <connor@codewithconnor.com>
2021-02-03 13:23:34 -0800

    :bug: Fixed issue with only first page paginated products being displayed

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -24,7 +24,7 @@ export const listProducts = (keyword = '', pageNumber = '') => async (dispatch) 
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST })
 
-    const { data } = await axios.get(`/api/products?keyword=${keyword}&page=${pageNumber}`)
+    const { data } = await axios.get(`/api/products?keyword=${keyword}&pageNumber=${pageNumber}`)
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
