005d10806e6cdaf798e9670561a8aa6aa7e85f6c
Connor Dillon <connor@codewithconnor.com>
2021-02-04 07:48:22 -0800

    :bug: Fixed breaking issue with product router

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,8 +4,8 @@ import { getProducts, getProductById, deleteProduct, updateProduct, createProduc
 import { protect, isAdmin } from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, isAdmin, createProduct)
+router.get('/top', getTopRatedProducts)
 router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct).put(protect, isAdmin, updateProduct)
 router.route('/:id/reviews').post(protect, createProductReview)
-router.route('/top', getTopRatedProducts)
 
 export default router
