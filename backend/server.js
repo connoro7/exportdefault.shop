c75a0207e48a6eca4744acca469e1099a14d1c9e
Connor Dillon <connor@codewithconnor.com>
2021-02-04 08:33:50 -0800

    :rocket: Reconfiguring server for production deploy

--- backend/server.js
+++ backend/server.js
@@ -21,37 +21,30 @@ app.use(express.json())
 if (process.env.NODE_ENV === 'DEVELOPMENT') {
   app.use(morgan('dev'))
 }
-// Mounts product routers onto the API at '/api/products' address
 app.use('/api/products', productRoutes)
-
-// Mounts user routers onto the API at '/api/users' address
 app.use('/api/users', userRoutes)
-
-// Mounts order routers onto the API at '/api/orders' address
 app.use('/api/orders', orderRoutes)
-
-// Mounts upload routers onto the API at '/api/upload' address
 app.use('/api/upload', uploadRoutes)
-
-app.get('/api/config/paypal', (request, response) => response.send(process.env.PAYPAL_CLIENT_ID))
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 // Hack to make __dirname accessible to ES Modules
 const __dirname = path.resolve()
-// Make /uploads folder static
+
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-app.use(notFound)
+if (process.env.NODE_ENV === 'PRODUCTION') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('API running :)')
+  })
+}
+
+app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
-
-app.get('/', (request, response) => {
-  response.send('API running :)')
-})
-
-app.get('*', (request, response) => {
-  response.sendFile(path.join(__dirname, '/../', 'build', 'index.html'))
-})
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
