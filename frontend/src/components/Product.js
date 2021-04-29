2d20c6c5be493a56920dbe845400a7bb7adf1bc8
Connor Dillon <connor@codewithconnor.com>
2021-04-28 23:44:02 -0700

    Fixed bad card layout on L & XL screens

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,20 +5,22 @@ import Rating from './Rating'
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className='mt-3 p-3 rounded h-100'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <Card.Img src={product.image} variant='top' className='mb-auto "img-fluid"' />
       </Link>
-      <Card.Body>
+      <Card.Body className='mb-n5'>
         <Link to={`/product/${product._id}`}>
-          <Card.Title as='div'>
+          <Card.Title as='h5'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
         <Card.Text as='div'>
           <Rating value={product.rating} text={`${product.numReviews} reviews`} />
         </Card.Text>
-        <Card.Text as='h3'>${product.price}</Card.Text>
+        <Card.Text as='h3' className='mb-auto'>
+          ${product.price}
+        </Card.Text>
       </Card.Body>
     </Card>
   )
