5258fb8f93555979b82f9f6b3154f84c7cc10500
Connor Dillon <connor@codewithconnor.com>
2023-01-03 17:58:22 -0800

    Changed default port from 5000 to 5050

--- backend/server.js
+++ backend/server.js
@@ -45,6 +45,6 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5050
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
